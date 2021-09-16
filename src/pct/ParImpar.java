ebe4348ec20c42aab043fddcf410fff7d662e505
B1-08 <B1-08@B1-08-PC>
2021-09-16 20:01:18 -0300

    Commit do tio do dia 16 de setembro. Par ou impar.

---
+++ src/pct/ParImpar.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pct;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author B1-08
+ */
+public class ParImpar {
+    public static void main(String[] args) {
+        //Declaração de variáveis
+        int n;
+        
+        //Criação e instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+        
+        
+        //Apresentação
+        System.out.println("\n\t\t\t -- Par ou Impar --\n");
+        
+        //Ler o número informado pelo usuário
+        System.out.print("\n\tInforme um número: ");
+        n = entrada.nextInt();
+        
+        
+        if((n%2) == 0)
+            System.out.println("\nO número " + n + " é par!");
+        
+    }
+    
+}
